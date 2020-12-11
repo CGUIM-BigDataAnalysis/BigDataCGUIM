ba370048bc0ab9160e4e6c4fb55a5b3b0069757a
yijutseng <beergt@gmail.com>
2020-12-11 22:09:38 +0800

    Data

---
+++ EMBA_BigData/lvrData.R
@@ -0,0 +1,34 @@
+library(readr)
+AllPreData<-NULL
+for (county in letters){
+  for(type in c("b")){ #"a",
+    path<-paste0("~/Downloads/lvr_landcsv/",county,"_lvr_land_",type,".csv")
+    if(file.exists(path)){
+      tempData <- read_csv(path)
+      tempData<-tempData[-1,]
+      AllPreData<-rbind(AllPreData,tempData)
+    }
+  }
+}
+
+AllOldData<-NULL
+for (county in letters){
+  for(type in c("a")){ #"a",
+    path<-paste0("~/Downloads/lvr_landcsv/",county,"_lvr_land_",type,".csv")
+    if(file.exists(path)){
+      tempData <- read_csv(path,
+                           col_types = cols(
+        .default = col_character(),
+        電梯 = col_character(),
+        主建物面積 = col_double(),
+        附屬建物面積 = col_double(),
+        陽台面積 = col_double()
+      ))
+      tempData<-tempData[-1,]
+      AllOldData<-rbind(AllOldData,tempData)
+    }
+  }
+}
+
+write_csv(AllPreData,"AllPreData.csv")
+write_csv(AllOldData,"AllOldData.csv")
