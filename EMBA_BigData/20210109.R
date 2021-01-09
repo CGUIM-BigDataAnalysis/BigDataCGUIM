b58d79178cae2d17515be7ebd55bc1cb8bbfb8f4
yijutseng <beergt@gmail.com>
2021-01-09 09:49:44 +0800

    Update 20210109.R

--- EMBA_BigData/20210109.R
+++ EMBA_BigData/20210109.R
@@ -6,3 +6,19 @@ library(reshape2)
 polio.m<-
   melt(polio,
        id.vars = c("YEAR","WEEK"))
+
+polio.m$value<-
+  as.numeric(polio.m$value)
+
+library(dplyr)
+polio.m.year<-polio.m %>% 
+  group_by(YEAR,variable) %>%
+  summarise(Inc=sum(value,na.rm = T))
+
+library(ggplot2)
+ggplot(polio.m.year,aes(YEAR,variable))+
+  geom_tile(aes(fill=Inc),color="white")+
+  scale_fill_gradient(low="white",
+                      high="red")+
+  theme_minimal()+
+  geom_vline(xintercept = 1955,size=1)
