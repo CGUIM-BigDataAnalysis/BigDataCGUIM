556b3df12ec9e95ae723cb6527bd08d468f62a4e
yijutseng <beergt@gmail.com>
2022-01-08 15:12:37 +0800

    rvest

--- EMBA_BigData/pttJob.R
+++ EMBA_BigData/pttJob.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(rvest)
 
 ptturl_main<-"https://www.ptt.cc/bbs/Tech_Job/"
 
@@ -9,11 +10,13 @@ lastpage_node<-html_nodes(ptt_html,".wide:nth-child(2)")
 #step 3 Clean and get the url
 lastpage_url<-html_attr(lastpage_node,"href")
 
-lastpage_url_split<-strsplit(lastpage_url,"index|.html")
+lastpage_url_split<-
+  strsplit(lastpage_url,"index|.html")
 lastpage_index<-lastpage_url_split[[1]][2]
 lastpage_index<-as.numeric(lastpage_index)
 
 all_titles<-c()
+
 for(i in (lastpage_index-9):(lastpage_index+1)){
   page_html<-read_html(paste0(ptturl_main,"index",i,".html"))
   titles <- page_html %>% html_nodes(".title a") %>% html_text()
