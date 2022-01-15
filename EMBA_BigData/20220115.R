30f54b68fb20a3306e113c48186444624b204d96
yijutseng <beergt@gmail.com>
2022-01-15 16:37:15 +0800

    water

--- EMBA_BigData/20220115.R
+++ EMBA_BigData/20220115.R
@@ -82,6 +82,30 @@ ggplot(iris,
   theme_bw()+
   labs(x="xxxxxxx",y="yyyyyy")
 
+
+
+library(jsonlite)
+WaterData<-fromJSON("http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=190796c8-7c56-42e0-8068-39242b8ec927")
+WaterDataFrame<-WaterData$result$results
+WaterDataFrame$longitude<-as.numeric(WaterDataFrame$longitude)
+WaterDataFrame$latitude<-as.numeric(WaterDataFrame$latitude)
+WaterDataFrame$qua_cntu<-as.numeric(WaterDataFrame$qua_cntu)
+WaterDataClean<-WaterDataFrame[WaterDataFrame$qua_cntu>=0,]
+
 #install.packages("ggmap") 
 library(ggmap)
 register_google(key = "AIzaSyAvNeyA_Uy8Z3JlzDnDuXQg7FhKFeVyNbY")
+
+TaipeiMap <- get_googlemap(
+  center  = c(lon=121.50,lat=25.06), 
+  zoom = 11, maptype = 'roadmap')
+TaipeiMapO <- ggmap(TaipeiMap)+ 
+  geom_point(data=WaterDataClean, 
+             aes(x=longitude, y=latitude,
+                 color=qua_cntu,size=3.5))+ 
+  scale_color_continuous(
+    low = "yellow",high = "red")+ 
+  guides(size="none")
+TaipeiMapO
+
+
