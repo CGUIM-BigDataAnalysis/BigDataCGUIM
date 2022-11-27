8f128fb4d4bb6bb6e90fa33084ed8acb3685e11c
yijutseng <beergt@gmail.com>
2022-11-27 14:55:58 +0800

    Update 20221127.R

--- EMBA_BigData/20221127.R
+++ EMBA_BigData/20221127.R
@@ -2,18 +2,28 @@
 install.packages("tidyverse")
 # load
 library(tidyverse)
+
+# variable 
 a<-1111111111
 22222->b
 c=999999
 
+# variable name
 TRUE
 True
 
-
+# Warning message
 log(-1)
 log(10)
+
+# Error message
 log()
+
+# Help
 ?mean
+
+
+
 
 ymd()
 
